000cb940c0d6095772eedf50cc77ceb370bf78d9
TheoB8B <btopikov@gmail.com>
2024-03-24 20:52:57 +0300

    add_pathP

--- test_py.py
+++ test_py.py
@@ -16,6 +16,7 @@ upper_edge = 5000
 pathx = 'TASCAM_Files/TASCAM_0035S1.wav'
 pathy = 'TASCAM_Files/TASCAM_0035S2.wav'
 pathz = 'TASCAM_Files/TASCAM_0035S3.wav'
+pathp = 'TASCAM_Files/TASCAM_0035S4.wav'
 T_beg = [931.75, 960.9, 988.6, 1019.3, 1049.7, 1079.9, 1110.1, 1140.8]
 T_end = [932.3, 961.4, 989.3, 1020.0, 1050.3, 1080.5, 1110.7, 1141.5]
 
@@ -38,6 +39,12 @@ for i in range(len(T_beg)):
 for i in range(len(T_beg)):
     print(f'Удар {i+1} по каналу Z')
     f, t, sxx = full_process(pathz, T_beg[i], T_end[i], NFFT)
+    data['t'].append(t)
+    data['sxx'].append(sxx)
+
+for i in range(len(T_beg)):
+    print(f'Удар {i+1} по каналу P')
+    f, t, sxx = full_process(pathp, T_beg[i], T_end[i], NFFT)
     data['t'].append(t)
     data['sxx'].append(sxx)
 
